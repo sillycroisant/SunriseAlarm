637647b806eeafd784f655d5d1284e06f2cc1911
sillycroisant <huynzodai@gmail.com>
2024-12-25 02:31:11 +0700

    thêm cái interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,17 @@
 #define LCD_SCL 0
 #define RTC_SDA 1
 #define RTC_SCL 0
-#define button_1 4
+#define button_1 18
 #define button_2 5
 #define button_3 6
 #define button_4 7
 #define ledPin 2
+#define debounce_delay 200
+
+volatile unsigned long last_button1_interrupt_time = 0;
+volatile unsigned long last_button2_interrupt_time = 0;
+volatile unsigned long last_button3_interrupt_time = 0;
+volatile unsigned long last_button4_interrupt_time = 0;
 
 //variablesvariables 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -25,20 +31,29 @@ int set_hour = 0, set_minute = 0;
 
 void displayTime();
 void getTime();
-void blinkLed();
+void turnOnLed();
+void turnOffLed();
 void wakeUp();
 int char2int(char c[]);
+void standoff();
 
 void setup() {
     //Setup mcu
     Serial.begin(115200);
 
     //Setup buttons and led (gpio)
-    // attachInterrupt(button_1, blinkLed, CHANGE);
-    // attachInterrupt(button_2, blinkLed, CHANGE);
-    // attachInterrupt(button_3, blinkLed, CHANGE);
-    // attachInterrupt(button_4, blinkLed, CHANGE);
+    pinMode(button_1, INPUT_PULLUP);
+    pinMode(button_2, INPUT_PULLUP);
+    pinMode(button_3, INPUT_PULLUP);
+    pinMode(button_4, INPUT_PULLUP);
+
+    attachInterrupt(button_1, turnOnLed, FALLING);
+    attachInterrupt(button_2, turnOffLed, FALLING);
+    attachInterrupt(button_3, standoff, FALLING);
+    attachInterrupt(button_4, standoff, FALLING);
+
     pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
 
     //Setup RTC
     Wire.begin(RTC_SDA,RTC_SCL);
@@ -60,15 +75,9 @@ void setup() {
 }
 
 void loop() {
-    //get the real time clock
     getTime();
-
-    //display the time and date
     displayTime();
-
-    //check whether it's the time to ring
     wakeUp();
-
     delay(3000);
 }
 
@@ -95,14 +104,28 @@ void getTime(){
     now = rtc.now();
 }
 
-void blinkLed(){
-    digitalWrite(ledPin, HIGH);
+void turnOnLed(){
+    unsigned long current_time = millis();
+    if(current_time - last_button1_interrupt_time > debounce_delay){
+        last_button1_interrupt_time = current_time;
+        digitalWrite(ledPin, HIGH);
+    }
+    last_button1_interrupt_time = current_time;
+}
+
+void turnOffLed(){
+    unsigned long current_time = millis();
+    if(current_time - last_button2_interrupt_time > debounce_delay){
+        last_button2_interrupt_time = current_time;
+        digitalWrite(ledPin, LOW);
+    }
+    last_button2_interrupt_time = current_time;
+
 }
 
 void wakeUp(){
     if(char2int(minute) == set_minute && char2int(hour) == set_hour){
-        blinkLed();
-        Serial.println("Wake up time");
+        digitalWrite(ledPin, HIGH);
     }
 }
 
@@ -110,4 +133,8 @@ int char2int(char c[]){
     String s = String(c);
     int i = s.toInt();
     return i;
+}
+
+void standoff(){
+
 }
