a3c5e22f706eccbbeffb1b4092c9a9c33dd12d17
sillycroisant <huynzodai@gmail.com>
2024-12-25 04:50:10 +0700

    add lunar date

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #include <SPI.h> 
 #include <LiquidCrystal_I2C.h> //used
 #include <RTClib.h> //used
+#include <TaskScheduler.h> //used
+#include <Date.h>
+#include <LunarConverter.h>
+
+using namespace Espace;
 
 // define pins
 #define LCD_SDA 1
@@ -26,16 +31,22 @@ volatile unsigned long last_button4_interrupt_time = 0;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 RTC_DS1307 rtc;
 DateTime now;
+Scheduler runner;
+
 char hour[16], minute[16], day[16], month[16];
 int set_hour = 0, set_minute = 0;
 
+
 void displayTime();
-void getTime();
-void turnOnLed();
-void turnOffLed();
+void getLunarDate();
+void IRAM_ATTR turnOnLed();
+void IRAM_ATTR turnOffLed();
 void wakeUp();
 int char2int(char c[]);
 void standoff();
+
+Task t1(60000, TASK_FOREVER, &displayTime); //every 60 seconds
+Task t2(86400000, TASK_FOREVER, &getLunarDate); //every 24 hours
 
 void setup() {
     //Setup mcu
@@ -70,41 +81,74 @@ void setup() {
     lcd.init();
     lcd.backlight();
      
+    //Setup task
+    runner.addTask(t1);
+    runner.addTask(t2);
+    t1.enable();
+    t2.enable();
+
+    //Lunar date
+    now = rtc.now();
+    Date solarDate(now.day(), now.month(), now.year()); //get solar date
+    LunarConverter converter;   //create a converter object
+    Date lunarDate = converter.Convert(solarDate); //convert solar date to lunar date
+
+    displayTime();
     //Whether the setup process is good
     Serial.print("The setup process was done!");
 }
 
 void loop() {
-    getTime();
-    displayTime();
-    wakeUp();
-    delay(3000);
+    runner.execute();
+    wakeUp(); //check if the time is the same as the set time
+}
+
+void getLunarDate(){
+    now = rtc.now();
+    Date solarDate(now.day(), now.month(), now.year());
+
+    LunarConverter converter;
+    Date lunarDate = converter.Convert(solarDate);
+    int dd = lunarDate.DayInMonth;
+    int mm = lunarDate.Month;
 }
 
 void displayTime(){
+    now = rtc.now();
+
+    Date solarDate(now.day(), now.month(), now.year()); //get solar date
+    LunarConverter converter;   //create a converter object
+    Date lunarDate = converter.Convert(solarDate); //convert solar date to lunar date
+
     lcd.setCursor(0,0);
-    lcd.print("    ");
     lcd.print(now.day(), DEC);
     lcd.print("/");
     lcd.print(now.month(), DEC);
     lcd.print("/");
     lcd.print(now.year(), DEC);
 
+
+    lcd.print(" ");
+    lcd.print(lunarDate.DayInMonth);
+    lcd.print("t");
+    lcd.print(lunarDate.Month);
+    lcd.print(" âm)");
+
     lcd.setCursor(0,1);
-    lcd.print("      ");
+    lcd.print("     ");
     sprintf(hour, "%02d", now.hour());
     lcd.print(hour);
     lcd.print(":");
     sprintf(minute, "%02d", now.minute());
     lcd.print(minute);
+    if(now.hour() >= 0 && now.hour() <= 12){
+        lcd.print(" AM");
+    } else {
+        lcd.print(" PM");
+    }
 }
 
-
-void getTime(){
-    now = rtc.now();
-}
-
-void turnOnLed(){
+void IRAM_ATTR turnOnLed(){
     unsigned long current_time = millis();
     if(current_time - last_button1_interrupt_time > debounce_delay){
         last_button1_interrupt_time = current_time;
@@ -113,7 +157,7 @@ void turnOnLed(){
     last_button1_interrupt_time = current_time;
 }
 
-void turnOffLed(){
+void IRAM_ATTR turnOffLed(){
     unsigned long current_time = millis();
     if(current_time - last_button2_interrupt_time > debounce_delay){
         last_button2_interrupt_time = current_time;
